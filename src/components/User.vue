a38aa83c16a1ec6cf75bb88830584edcf27aed19
gersonEgues <gerson.choque.loreno@gmail.com>
2023-12-28 21:29:28 -0400

    remove extra components

---
+++ src/components/User.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import {ref,onMounted} from 'vue'
+
+    const count = ref(0)
+
+    function increment(value:number) {
+        count.value++;
+    }
+
+    onMounted(()=>{
+        console.log(`the initial count is ${count.value}`);
+    });
+</script>
+
+<template>
+    <h1>holaaaa</h1>
+
+    <button>test click</button>
+</template>
+
+<style>
+</style>

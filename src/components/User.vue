57abea6a971da53c89038c0bdf787a4057a4bdc3
gersonEgues <gerson.choque.loreno@gmail.com>
2023-12-30 15:05:27 -0400

    axios at FE and cors at BE was added

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
     import {ref,onMounted} from 'vue'
+    import axios from "axios";
+
 
     let contador = 0;
 
@@ -9,7 +11,13 @@
     }
 
     function getRoles(){
-
+        axios.get('http://localhost:8083/api/rol/list')
+        .then(res=>{
+            console.log(res);
+        })
+        .catch(err=>{
+            console.log(err);
+        });
     }
 
     onMounted(()=>{
@@ -19,15 +27,27 @@
 </script>
 
 <template>
-    <div class="card">
-        <h1>holaaaa</h1>
+  <div class="card">
+    <label class="m-3 fw-bold">DATOS DEL USUARIO</label>
+    <form class="m-3">
+      <div class="mb-3">
+        <label for="userName" class="form-label">User Name</label>
+        <input type="text" class="form-control" id="userName">
+      </div>
+      
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password">
+      </div>
 
-        <button type="button" 
-                class="btn btn-primary"
-                @click="increment">
-            Incrementar
-        </button>
-    </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control " id="email">
+      </div>
+                
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>     
+  </div>
 </template>
 
 <style>
